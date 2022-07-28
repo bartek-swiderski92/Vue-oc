51fbcd58ce8723e82f839d5eb746a3fb52830622
Bartek Swiderski <bartek.swiderski@corestream.co.uk>
2022-07-28 13:04:23 +0100

    add beforeMount

--- cafe-with-a-vue/cli-version/src/components/MenuItem.vue
+++ cafe-with-a-vue/cli-version/src/components/MenuItem.vue
@@ -1,7 +1,27 @@
 <script>
 export default {
-  name: 'MenuItem',
-  props: ['addToShoppingCart', 'image', 'inStock', 'name', 'price', 'quantity']
+  name: "MenuItem",
+  props: ["addToShoppingCart", "image", "inStock", "name", "price", "quantity"],
+  data() {
+    return {
+      onSale: true
+    }
+  },
+  computed: {
+    generatedPrice() {
+      if (this.onSale) {
+        return (this.price * 0.9).toFixed(2)
+      } else {
+        return this.price
+      }
+    }
+  },
+  beforeMount() {
+    const today = new Date().getDate()
+    if (today % 2 === 0) {
+      this.onSale = true
+    }
+  }
 }
 </script>
 
@@ -10,15 +30,13 @@ export default {
     <img class="menu-item__image" :src="image.source" :alt="image.alt" />
     <div>
       <h3>{{ name }}</h3>
-      <p>Price: {{ price }}</p>
+      <p>Price: {{ generatedPrice  }} <span v-if="onSale">(10% off!)</span></p>
       <p v-if="inStock">In Stock</p>
       <p v-else>Out of Stock</p>
       <div>
         <label for="add-item-quantity">Quantity: {{ quantity }}</label>
         <input v-model.number="quantity" id="add-item-quantity" type="number" />
-        <button @click="addToShoppingCart(quantity)">
-          Add to Shopping Cart
-        </button>
+        <button @click="addToShoppingCart(quantity)">Add to Shopping Cart</button>
       </div>
     </div>
   </div>
