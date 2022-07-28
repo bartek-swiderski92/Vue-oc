a64703c2f94f065402dbe22809917bf361174a3c
Bartek Swiderski <bartek.swiderski@corestream.co.uk>
2022-07-28 11:09:20 +0100

    move files to CLI

--- cafe-with-a-vue/cli-version/src/App.vue
+++ cafe-with-a-vue/cli-version/src/App.vue
@@ -1,18 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <h1>{{ restaurantName }}</h1>
+      <p class="description">
+        Welcome to {{ restaurantName }}! We are known for our freshly baked
+        bread and french pastries! Give you morning a warm start or treat
+        yourself in the middle of the day. Our butter is imported from local
+        farmers in France. Once you take your first bite, you will see why
+        everyone can't get enough!
+      </p>
+
+      <section class="menu">
+        <h2>Menu</h2>
+        <div v-for="item in simpleMenu" :key="item.name" class="menu-item">
+          <img
+            class="menu-item__image"
+            :src="item.image.source"
+            :alt="item.image.alt"
+          />
+          <div>
+            <h3>{{ item.name }}</h3>
+            <p v-if="item.inStock">In Stock</p>
+            <p v-else>Out of Stock</p>
+            <div>
+              <label for="add-item-quantity"
+                >Quantity: {{ item.quantity }}</label
+              >
+              <input
+                v-model.number="item.quantity"
+                id="add-item-quantity"
+                type="number"
+              />
+              <button @click="addToShoppingCart(item.quantity)">
+                Add to Shopping Cart
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <aside class="shopping-cart">
+        <h2>Shopping Cart: {{ shoppingCart }} items</h2>
+      </aside>
+
+      <h2>Contact Us</h2>
+      <p>Address: {{ address }}</p>
+      <p>Phone: {{ phone }}</p>
+      <p>Email: {{ email }}</p>
+      <p>Hours:</p>
+      <ul>
+        <li>M-F: 06:00 to 16:00</li>
+        <li>Saturday: 07:00 to 14:00</li>
+        <li>Sunday: 07:00 to 12:00</li>
+      </ul>
+      <footer class="footer">
+        <p>{{ copyright }}</p>
+      </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+
+  },
 }
 </script>
 
---
+++ cafe-with-a-vue/cli-version/src/components/MenuItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <img class="menu-item__image" :src="imageSource" :alt="imageAlt" />
+    <div>
+      <h3>{{ itemName }}</h3>
+      <p v-if="isInStock">In Stock</p>
+      <p v-else>Out of Stock</p>
+      <div>
+        <label for="add-item-quantity">Quantity: {{ itemQuantity }}</label>
+        <input
+          v-model.number="itemQuantity"
+          id="add-item-quantity"
+          type="number"
+        />
+        <button @click="addToShoppingCart(itemQuantity)">Add to Shopping Cart</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MenuItem",
+  props: [
+    addToShoppingCart,
+    imageSource,
+    imageAlt,
+    itemName,
+    isInStock,
+    itemQuantity
+  ]
+};
+</script>
+
+<style>
+</style>
